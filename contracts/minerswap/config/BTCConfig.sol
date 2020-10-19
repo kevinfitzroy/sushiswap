2090e3da1a9ff28e725fde5267aa3ba6324236a6
Kevin Fitzroy <kevinfitzroy715@gmail.com>
2020-10-19 15:34:36 +0800

    add real wrapper btc contract address

--- contracts/minerswap/config/BTCConfig.sol
+++ contracts/minerswap/config/BTCConfig.sol
@@ -7,10 +7,9 @@ contract BTCConfig is Ownable{
     mapping(string => address) public btcAddress;
 
     constructor() public {
-        btcAddress["wBTC"] = address(0);
-        btcAddress["hBTC"] = address(0);
-        btcAddress["sBTC"] = address(0);
-        btcAddress["renBTC"] = address(0);
+        btcAddress["wBTC"] = address(0x2260fac5e5542a773aa44fbcfedf7c193bc2c599));
+        btcAddress["hBTC"] = address(0x0316EB71485b0Ab14103307bf65a021042c6d380);
+        btcAddress["sBTC"] = address(0xfe18be6b3bd88a2d2a7f928d00292e7a9963cfc6);
     }
 
     function update(string memory name, address addr) public onlyOwner{
