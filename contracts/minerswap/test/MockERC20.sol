7d5ebdc621cb33e7c82d2e6566cbdf57a48d9132
Kevin Fitzroy <kevinfitzroy715@gmail.com>
2020-10-21 15:52:43 +0800

    add mine func to MockERC20

--- contracts/minerswap/test/MockERC20.sol
+++ contracts/minerswap/test/MockERC20.sol
@@ -8,4 +8,8 @@ contract MockERC20 is ERC20 {
         string memory _symbol
     ) public ERC20(_name, _symbol) {
     }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
 }
